5e5e2ba03a2bb63185dbba21c0c5202da810b858
curtywang <ycwang@u.northwestern.edu>
2016-08-13 21:10:52 -0500

    Wrote version 0.1 of TemperatureMeasurementController class

---
+++ nsf_switch_control/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+using NationalInstruments;
+using NationalInstruments.ModularInstruments.NISwitch;
+using NationalInstruments.ModularInstruments.SystemServices.DeviceServices;
+using NationalInstruments.Visa;
+using NationalInstruments.DAQmx;
+
+namespace NsfSwitchControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadSwitchDeviceNames();
+        }
+
+        private void LoadSwitchDeviceNames()
+        {
+            ModularInstrumentsSystem modularInstrumentsSystem = new ModularInstrumentsSystem();//"NI-SWITCH");
+            foreach (DeviceInfo device in modularInstrumentsSystem.DeviceCollection)
+            {
+                Console.WriteLine(device.Name);
+            }
+        }
+    }
+
+    // TODO: Write SwitchController class (which is NISwitch to the PXIe-2529)
+    public class SwitchController
+    { }
+
+
+    // TODO: Write ImpedanceMeasurementController class (which is just VISA to the HM8118)
+    public class ImpedanceMeasurementController
+    { }
+
+
+    public class TemperatureMeasurementController
+    {
+        // TODO: sync with the impedance reader so a temperature reading occurs the same time as a impedance measurement 
+        // (I think if we just start both at the same time, since one is at 2 Hz, then the other should just switch within 0.25 seconds and have a reading at 0.5 seconds)
+        // (of course it's largely dependent on the max speed of the LCR meter) 
+
+        private AnalogMultiChannelReader analogInReader;
+        private AsyncCallback myAsyncCallback;
+        private AnalogWaveform<double>[] data;
+        private DataColumn[] dataColumn = null;
+        private DataTable dataTable = null;
+        private NationalInstruments.DAQmx.Task myTask;
+        private NationalInstruments.DAQmx.Task runningTask;
+        private List<string> channelsToUseAddresses = new List<string>();
+        System.IO.StreamWriter dataWriteFile;
+
+        // RTD physical configuration
+        static private List<int> __channelsToUse = new List<int>(Enumerable.Range(0, 20)); // use all of the channels
+        static private string __pxiLocation = "PXI1Slot3";
+        static private AIRtdType __rtdType = AIRtdType.Pt3851;
+        static private double __r0Numeric = 100.0;
+        static private AIResistanceConfiguration __resistanceConfiguration = AIResistanceConfiguration.ThreeWire;
+        static private AIExcitationSource __excitationSource = AIExcitationSource.Internal;
+        static private double __minimumValueNumeric = 0.0;
+        static private double __maximumValueNumeric = 200.0;
+        static private AITemperatureUnits __temperatureUnit = AITemperatureUnits.DegreesC;
+        static private double __currentExcitationNumeric = 900e-6;
+
+        // Sampling configuration
+        static private double __sampleRate = 2.0; // in Hz
+        static private int __samplesPerChannelBeforeRelease = 1;
+
+        // Data table configuration
+        static private string __dataTableHeader = "date,time," + String.Join(",", __channelsToUse);
+
+
+        public TemperatureMeasurementController(string saveFileLocation)
+        {
+            foreach (int channelId in __channelsToUse)
+                channelsToUseAddresses.Add(__pxiLocation + "/ai" + channelId.ToString());
+            dataWriteFile = new System.IO.StreamWriter(saveFileLocation);
+            dataWriteFile.WriteLine(__dataTableHeader);
+        }
+
+
+        // function called to start measurements
+        public void StartMeasurement()
+        {
+            try
+            {
+                myTask = new NationalInstruments.DAQmx.Task();
+                myAsyncCallback = new AsyncCallback(AnalogInCallback);
+
+                // tell the PXIe-4537 to use the channels in channelsToUse
+                foreach (string channel in channelsToUseAddresses)
+                {
+                    myTask.AIChannels.CreateRtdChannel(channel, "", __minimumValueNumeric, __maximumValueNumeric,
+                        __temperatureUnit, __rtdType, __resistanceConfiguration, __excitationSource,
+                        __currentExcitationNumeric, __r0Numeric);
+                }
+
+                // tell the PXIe-4537 to use its internal sample clock at some sample rate
+                myTask.Timing.ConfigureSampleClock("", __sampleRate, SampleClockActiveEdge.Rising,
+                    SampleQuantityMode.ContinuousSamples, __samplesPerChannelBeforeRelease);
+
+                // check if the PXIe-4537 likes our settings
+                myTask.Control(TaskAction.Verify);
+
+                // declare the reader object for the task and prepare to run the task
+                analogInReader = new AnalogMultiChannelReader(myTask.Stream);
+                runningTask = myTask;
+
+                // Use SynchronizeCallbacks to specify that the object 
+                // marshals callbacks across threads appropriately.
+                analogInReader.SynchronizeCallbacks = true;
+                analogInReader.BeginReadWaveform(__samplesPerChannelBeforeRelease, myAsyncCallback, myTask);
+            }
+            catch (DaqException exception)
+            {
+                ErrorCatcher(exception);
+            }
+        }
+
+
+        // function called by async when data comes in from the PXIe-4537
+        // basically it just reads and then writes to the file
+        private void AnalogInCallback(IAsyncResult ar)
+        {
+            try
+            {
+                if (runningTask != null && runningTask == ar.AsyncState)
+                {
+                    AnalogWaveform<double>[] data = analogInReader.EndReadWaveform(ar);
+                    DataWrite(data);
+                    analogInReader.BeginMemoryOptimizedReadWaveform(__samplesPerChannelBeforeRelease, myAsyncCallback, myTask, data);
+                }
+            }
+            catch (DaqException exception)
+            {
+                ErrorCatcher(exception);
+            }
+        }
+
+
+        // convert the data from the AnalogWaveform to a string to write
+        private void DataWrite(AnalogWaveform<double>[] sourceArray)
+        {
+            try
+            {
+                if (sourceArray.Length != __channelsToUse.Count)
+                    throw new System.DataMisalignedException("Error: the incoming data has a different size than the number of channels!");
+
+                string currentDate= DateTime.Now.ToLongDateString();
+                string currentTime = DateTime.Now.ToLongTimeString();
+
+                // assume sourceArray has channels in the original order
+                foreach (int sample in Enumerable.Range(0, __samplesPerChannelBeforeRelease))
+                {
+                    string currentLine = currentDate + "," + currentTime + ","; //String.Join(",", sourceArray);
+                    double[] sampleData = new double[__channelsToUse.Count];
+                    foreach (int channel in __channelsToUse)
+                    {
+                        sampleData[channel] = sourceArray[channel].Samples[sample].Value;
+                    }
+                    currentLine += String.Join(",", sampleData);
+                    dataWriteFile.WriteLine(currentLine);
+                }
+            }
+            catch (System.DataMisalignedException dmex)
+            {
+                MessageBox.Show(dmex.Message);
+            }
+        }
+
+
+        // generic error handler to display any messages from DAQmx
+        private void ErrorCatcher(DaqException exception)
+        {
+            MessageBox.Show(exception.Message);
+            myTask.Dispose();
+            runningTask = null;
+        }
+    }
+}
